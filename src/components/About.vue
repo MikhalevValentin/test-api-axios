64256b9eaf699761205f7efc9c30b0c4ccc372af
Valentin Mikhalev <Valentin.Mikhalev@mirapolis.ru>
2017-06-23 12:05:59 +0300

    add files

---
+++ src/components/About.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="about">
+    <ul class="wrap-article" v-for="result in results">
+      <li class="id-info">
+        <span>id пользователя: {{ result.userId }}</span><br>
+        <span>id статьи: {{ result.id }}</span>
+      </li>
+      <li class="article-title">{{ result.title }}</li>
+      <li class="article-body">
+        <p>{{ result.body }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+      results: []
+    }
+  },
+  mounted() {
+    axios.get("http://jsonplaceholder.typicode.com/posts")
+    .then(response => {this.results = response.data})
+    .catch(e => {this.errors.push(e)})
+  },
+}
+</script>
+<!-- styling for the component -->
+<style>
+#about {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.wrap-article {
+  display: block;
+  background-color: #eee;
+  padding: 10px 20px;
+  list-style-type: none;
+  text-align: left;
+  margin-bottom: 20px;
+}
+
+.article-title {
+  font-size: 22px;
+  margin-bottom: 10px;
+}
+
+p {
+  margin: 0;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="hello">
+    <h3>{{ msg }}</h3>
+    <h4>links</h4>
+    <section class="block-img" v-for="album in albums">
+      <div class="img-wrap">
+        <img :src="album.thumbnailUrl" :alt="album.title ">
+      </div>
+      <ul>
+        <li class="id-info">
+          <span>id пользователя: {{ album.albumId }}</span><br>
+          <span>id статьи: {{ album.id }}</span>
+        </li>
+        <li class="article-title">{{ album.title }}</li>
+        <li class="article-body">
+          <a :href="album.url" class="album-link">{{ album.url }}</a>
+        </li>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'root',
+      albums: []
+    }
+  },
+  mounted() {
+    axios.get("http://jsonplaceholder.typicode.com/photos")
+    .then(response => {this.albums = response.data})
+    .catch(e => {this.errors.push(e)})
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.wrap-article {
+  display: block;
+  width: 100%;
+  padding: 10px;
+}
+
+.id-info {
+  display: block;
+  font-size: 12px;
+  color: green;
+}
+
+.article-title {
+  display: block;
+  font-size: 22px;
+}
+
+.article-body {
+  font-size: 16px;
+  padding: 0;
+}
+
+p {
+  margin: 10px 0 0;
+}
+
+.block-img {
+  display: block;
+  margin-bottom: 20px;
+  padding: 15px 20px;
+  background-color: #eee;
+}
+
+.img-wrap img {
+  display: block;
+  width: 150px;
+  height: 150px;
+  border: 4px solid #454546
+}
+</style>
